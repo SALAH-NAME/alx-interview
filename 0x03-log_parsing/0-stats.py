5df251546b5dc4019cb6a7b04bff5c21433ff37d
Salah <Salah>
2024-08-28 11:40:51 +0100

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,38 +11,32 @@ def print_stats(total_size, status_counts):
             print(f"{status}: {status_counts[status]}")
 
 
-file_size = 0
-code = 0
-line_count = 0
-codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
+total_size = 0
+status_counts = {
+        200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
 }
+line_count = 0
+
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
+        parts = line.split()
+        if len(parts) < 7:
+            continue
 
-        if len(parsed_line) > 2:
-            line_count += 1
+        try:
+            file_size = int(parts[-1])
+            status_code = int(parts[-2])
+        except ValueError:
+            continue
 
-            if line_count <= 10:
-                file_size += int(parsed_line[0])
-                code = parsed_line[1]
+        total_size += file_size
+        if status_code in status_counts:
+            status_counts[status_code] += 1
 
-                if (code in codes.keys()):
-                    codes[code] += 1
+        line_count += 1
 
-            if (line_count == 10):
-                print_stats(codes, file_size)
-                line_count = 0
-
+        if line_count % 10 == 0:
+            print_stats(total_size, status_counts)
 finally:
-    print_stats(codes, file_size)
+    print_stats(total_size, status_counts)
