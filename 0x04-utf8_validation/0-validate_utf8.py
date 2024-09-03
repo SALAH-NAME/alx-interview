5b408fd90bd9b460fa4444b3993ae09190c399d2
Salah <Salah>
2024-09-03 13:32:40 +0100

    uft8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,25 +8,20 @@ def validUTF8(data):
     """
     num_bytes = 0
 
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-
     for num in data:
         byte = num & 0xFF
 
         if num_bytes == 0:
-            if (byte & mask1) == 0:
-                continue
-            elif (byte & (mask1 >> 1)) == mask1:
+            if (byte >> 5) == 0b110:
                 num_bytes = 1
-            elif (byte & (mask1 >> 2)) == (mask1 >> 1):
+            elif (byte >> 4) == 0b1110:
                 num_bytes = 2
-            elif (byte & (mask1 >> 3)) == (mask1 >> 2):
+            elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            else:
+            elif (byte >> 7):
                 return False
         else:
-            if not (byte & mask1 and not (byte & mask2)):
+            if (byte >> 6) != 0b10:
                 return False
         num_bytes -= 1
 
