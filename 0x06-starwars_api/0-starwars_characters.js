5efff92418a9152eb1e68600d989a8289879e0a0
Salah <Salah>
2024-09-20 11:15:15 +0100

    starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,19 +3,24 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(apiUrl, function (err, res, body) {
-  if (err) throw err;
+request(apiUrl, async (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
   const characters = JSON.parse(body).characters;
-  printCharactersInOrder(characters, 0);
+  for (const characterUrl of characters) {
+    await new Promise((resolve, reject) => {
+      request(characterUrl, (err, res, body) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
-
-const printCharactersInOrder = (characters, index) => {
-  if (index === characters.length) return;
-  request(characters[index], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    printCharactersInOrder(characters, index + 1);
-  });
-};
