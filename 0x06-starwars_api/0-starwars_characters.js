5661a33d30861458f016b8f8c649d224809e5a0a
Salah <Salah>
2024-09-20 11:21:01 +0100

    starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,17 +5,17 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(apiUrl, async (err, res, body) => {
-  if (err) {
-    console.error(err);
+request(apiUrl, async (error, res, body) => {
+  if (error) {
+    console.error(error);
     return;
   }
   const characters = JSON.parse(body).characters;
   for (const characterUrl of characters) {
     await new Promise((resolve, reject) => {
-      request(characterUrl, (err, res, body) => {
-        if (err) {
-          reject(err);
+      request(characterUrl, (error, res, body) => {
+        if (error) {
+          reject(error);
           return;
         }
         console.log(JSON.parse(body).name);
