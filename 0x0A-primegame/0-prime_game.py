eb47db700e03dfa57efe8f5ffcfe765aa551528a
Salah <Salah>
2024-10-16 11:45:33 +0100

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,34 +13,42 @@ def is_prime(n):
     return True
 
 
-def calculate_winner(n):
-    """calculate_winner function
+def generate_primes(limit):
+    """generate_primes function
     """
-    primes = [i for i in range(2, n + 1) if is_prime(i)]
-    maria_wins = 0
-    ben_wins = 0
-    for i in range(len(primes)):
-        if i % 2 == 0:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-    return maria_wins, ben_wins
+    primes = [True] * (limit + 1)
+    primes[0] = primes[1] = False
+    for i in range(2, int(limit ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, limit + 1, i):
+                primes[j] = False
+    return primes
+
+
+def count_primes(primes, n):
+    """count_primes function
+    """
+    return sum(primes[:n + 1])
 
 
 def isWinner(x, nums):
     """isWinner function
     """
-    maria_total = 0
-    ben_total = 0
-    for n in nums:
-        maria_wins, ben_wins = calculate_winner(n)
-        if maria_wins > ben_wins:
-            maria_total += 1
-        else:
-            ben_total += 1
-    if maria_total > ben_total:
-        return "Maria"
-    elif ben_total > maria_total:
-        return "Ben"
-    else:
+    if x < 1 or not nums:
         return None
+
+    maria_wins, ben_wins = 0, 0
+    max_num = max(nums)
+    primes = generate_primes(max_num)
+
+    for _, current_num in zip(range(x), nums):
+        prime_count = count_primes(primes, current_num)
+        if prime_count % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins == ben_wins:
+        return None
+
+    return 'Maria' if maria_wins > ben_wins else 'Ben'
